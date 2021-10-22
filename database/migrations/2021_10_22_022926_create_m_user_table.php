a19afba3bbbb26d91a6d7507e4aed635a5d28d51
AyeMyaWai <61411011+AyeMyaWai@users.noreply.github.com>
2021-10-22 10:58:56 +0630

    table

--- database/migrations/2021_10_22_022926_create_m_user_table.php
+++ database/migrations/2021_10_22_022926_create_m_user_table.php
@@ -15,23 +15,24 @@ class CreateMUserTable extends Migration
     {
         Schema::create('m_user', function (Blueprint $table) {
             $table->id();
-                $table->integer('membership_no', 10)->comment('会員番号(stores予約)');
-                $table->integer('customer_no', 50)->comment('顧客番号');
-                $table->string('reservation_customer_nm', 10)->comment('予約者の氏名');
-                $table->string('customer_nm', 50)->comment('顧客名');
-                $table->string('mail_address')->comment('メールアドレス');
-                $table->string('telephone_no', 50)->comment('電話番号');
-                $table->string('birthday', 50)->comment('誕生日');
-                $table->string('sex', 50)->comment('性別');
-                $table->string('postal_code', 50)->comment('郵便番号');
-                $table->string('customer_memo', 50)->comment('顧客メモ');
-                $table->string('registeration_date', 50)->comment('登録日');
-                $table->string('registered_person', 50)->comment('登録者');
-                $table->dateTime('data_i_ymd')->comment('登録日');
-                $table->string('data_i_user', 100)->comment('登録者');
-                $table->dateTime('data_u_ymd')->comment('更新日');
-                $table->string('data_u_user',100)->comment('更新者');
+            $table->integer('membership_no', 10)->comment('会員番号(stores予約)');
+            $table->integer('customer_no', 50)->comment('顧客番号');
+            $table->string('reservation_customer_nm', 10)->comment('予約者の氏名');
+            $table->string('customer_nm', 50)->comment('顧客名');
+            $table->string('mail_address')->comment('メールアドレス');
+            $table->string('telephone_no', 50)->comment('電話番号');
+            $table->date('birthday', 50)->comment('誕生日');
+            $table->string('sex', 50)->comment('性別');
+            $table->string('postal_code', 50)->comment('郵便番号');
+            $table->string('customer_memo', 50)->comment('顧客メモ');
+            $table->string('registeration_date', 50)->comment('登録日');
+            $table->string('registered_person', 50)->comment('登録者');
+            $table->dateTime('data_i_ymd')->comment('登録日');
+            $table->string('data_i_user', 100)->comment('登録者');
+            $table->dateTime('data_u_ymd')->comment('更新日');
+            $table->string('data_u_user',100)->comment('更新者');
             $table->timestamps();
+            $table->unique(['user_id']);
         });
     }
 
--- database/migrations/2021_10_22_041618_create_k_m_user_table.php
+++ database/migrations/2021_10_22_041618_create_k_m_user_table.php
@@ -14,8 +14,20 @@ class CreateKMUserTable extends Migration
     public function up()
     {
         Schema::create('k_m_user', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('user_id', 36)->comment('ユーザID ※UUID');
+            $table->string('user_name', 100)->comment('ユーザ名');
+            $table->string('password', 128)->comment('パスワード');
+            $table->string('mailaddress', 100)->comment('メールアドレス');
+            $table->text('remarks')->comment('備考')->nullable();
+            $table->dateTime('data_i_ymd')->comment('登録日');
+            $table->string('data_i_user', 100)->comment('登録者');
+            $table->dateTime('data_u_ymd')->comment('更新日');
+            $table->string('data_u_user', 100)->comment('更新者');
+            $table->rememberToken();
             $table->timestamps();
+            // UKの設定
+            $table->unique(['user_id']);
         });
     }
 
